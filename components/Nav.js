4f163b51b33bee5220e5b675f4c870b9bd83b46c
Jose Cerqueira <jose@origensmedia.com>
2019-06-04 22:57:58 +0100

    Set up the UI for when an user logs in

--- components/Nav.js
+++ components/Nav.js
@@ -1,9 +1,19 @@
 import Link from "next/link";
 
 import NavStyles from "./styles/NavStyles";
+import User from "./User";
 
 const Nav = () => (
 	<NavStyles>
+		<User>
+			{({ data: { me } }) => {
+				console.log(me);
+				if (me) return <p>{me.name}</p>;
+				return null;
+				// if (me) return <p>{me.name}</p>;
+				// return <p>User</p>;
+			}}
+		</User>
 		<Link href="/items">
 			<a>Shop</a>
 		</Link>
