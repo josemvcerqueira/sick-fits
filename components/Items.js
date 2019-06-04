08970107f7fede0318c2ce76e4807e8c5f5689ba
Jose Cerqueira <jose@origensmedia.com>
2019-06-04 16:47:12 +0100

    functionality to fetch the right items per page

--- components/Items.js
+++ components/Items.js
@@ -5,10 +5,11 @@ import styled from "styled-components";
 
 import Item from "./Item";
 import Pagination from "./Pagination";
+import { perPage } from "../config";
 
 const ALL_ITEMS_QUERY = gql`
-	query ALL_ITEMS_QUERY {
-		items {
+	query ALL_ITEMS_QUERY($skip: Int = 0, $first: Int = ${perPage} ) {
+		items(first: $first, skip: $skip, orderBy: createdAt_DESC) {
 			id
 			title
 			price
@@ -38,7 +39,12 @@ class Items extends Component {
 			<Center>
 				<Pagination page={page} />
 				<p>Items</p>
-				<Query query={ALL_ITEMS_QUERY}>
+				<Query
+					query={ALL_ITEMS_QUERY}
+					variables={{
+						skip: page * perPage - perPage
+					}}
+				>
 					{({ data, error, loading }) => {
 						if (loading) return <p>Loading...</p>;
 						if (error) return <p>{error.message}</p>;
