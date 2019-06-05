24eb475bec83911453df6c5d9dbcddcc5be1a54d
Jose Cerqueira <jose@origensmedia.com>
2019-06-05 14:30:46 +0100

    Set up the Please Signin component

---
+++ components/PleaseSignin.js
@@ -0,0 +1,23 @@
+import { Query } from "react-apollo";
+
+import { CURRENT_USER_QUERY } from "./User";
+import Signin from "./Signin";
+
+const PleaseSignin = ({ children }) => (
+	<Query query={CURRENT_USER_QUERY}>
+		{({ data, loading }) => {
+			if (loading) return <p>Loading...</p>;
+			if (!data.me) {
+				return (
+					<div>
+						<p>Please Sign In before continuing</p>
+						<Signin />
+					</div>
+				);
+			}
+			return children;
+		}}
+	</Query>
+);
+
+export default PleaseSignin;
--- pages/sell.js
+++ pages/sell.js
@@ -1,5 +1,10 @@
+import PleaseSignIn from "../components/PleaseSignin";
 import CreateItem from "../components/CreateItem";
 
-const Sell = () => <CreateItem />;
+const Sell = () => (
+	<PleaseSignIn>
+		<CreateItem />
+	</PleaseSignIn>
+);
 
 export default Sell;
