eb768a8da5165d1f62bbc3686a91e341f3613937
Jose Cerqueira <jose@origensmedia.com>
2019-06-05 09:09:28 +0100

    Set up the UI to signout

--- components/Nav.js
+++ components/Nav.js
@@ -1,6 +1,7 @@
 import Link from "next/link";
 
 import NavStyles from "./styles/NavStyles";
+import Signout from "./Signout";
 import User from "./User";
 
 const Nav = () => (
@@ -21,6 +22,7 @@ const Nav = () => (
 						<Link href="/me">
 							<a>Account</a>
 						</Link>
+						<Signout />
 					</>
 				)}
 				{!me && (
