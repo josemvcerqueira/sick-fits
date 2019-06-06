b728fd3cc8cd019e34664dffa76eda16a9efe867
Jose Cerqueira <jose@origensmedia.com>
2019-06-06 17:16:33 +0100

    Set up local state to the apollo store for the cart

--- components/Nav.js
+++ components/Nav.js
@@ -1,5 +1,7 @@
 import Link from "next/link";
+import { Mutation } from "react-apollo";
 
+import { TOGGLE_CART_MUTATION } from "./Cart";
 import NavStyles from "./styles/NavStyles";
 import Signout from "./Signout";
 import User from "./User";
@@ -23,6 +25,11 @@ const Nav = () => (
 							<a>Account</a>
 						</Link>
 						<Signout />
+						<Mutation mutation={TOGGLE_CART_MUTATION}>
+							{toggleCart => (
+								<button onClick={toggleCart}>My Cart</button>
+							)}
+						</Mutation>
 					</>
 				)}
 				{!me && (
