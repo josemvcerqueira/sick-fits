2c82a208bdf2be543aeaa7a3723f0d5536f50bc3
Jose Cerqueira <jose@origensmedia.com>
2019-06-07 12:21:56 +0100

    Improve the UI to remove a cart Item

--- components/RemoveFromCart.js
+++ components/RemoveFromCart.js
@@ -28,10 +28,36 @@ class RemoveFromCart extends PureComponent {
 	static propTypes = {
 		id: PropTypes.string.isRequired
 	};
+
+	// This gets called as soon as we get a response back from the server after a mutation has been performed
+	update = (cache, payload) => {
+		// 1. First read the cache
+		const data = cache.readQuery({ query: CURRENT_USER_QUERY });
+		// 2. Remove that item from the cart
+		const cartItemId = payload.data.removeFromCart.id;
+		data.me.cart = data.me.cart.filter(
+			cartItem => cartItem.id !== cartItemId
+		);
+		// 3. Write it back to the cache
+		cache.writeQuery({ query: CURRENT_USER_QUERY, data });
+	};
+
 	render() {
 		const { id } = this.props;
+		const { update } = this;
 		return (
-			<Mutation mutation={REMOVE_FROM_CART_MUTATION} variables={{ id }}>
+			<Mutation
+				mutation={REMOVE_FROM_CART_MUTATION}
+				variables={{ id }}
+				update={update}
+				optimisticResponse={{
+					__typename: "Mutation",
+					removeFromCart: {
+						__typename: "CartItem",
+						id: this.props.id
+					}
+				}}
+			>
 				{(removeFromCart, { loading }) => {
 					return (
 						<BigButton
