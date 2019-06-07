dfbaf38c6ac35ab0f566cf4936b0cc0896268398
Jose Cerqueira <jose@origensmedia.com>
2019-06-07 16:48:15 +0100

    Set up the query and UI for the search bar

--- components/Header.js
+++ components/Header.js
@@ -5,6 +5,7 @@ import NProgress from "nprogress";
 import styled from "styled-components";
 
 import Nav from "./Nav";
+import Search from "./Search";
 
 Router.onRouteChangeStart = () => {
 	NProgress.start();
@@ -66,7 +67,7 @@ const Header = () => {
 				<Nav />
 			</div>
 			<div className="sub-bar">
-				<p>Search</p>
+				<Search />
 			</div>
 			<Cart />
 		</StyledHeader>
