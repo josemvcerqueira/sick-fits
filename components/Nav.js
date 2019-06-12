42755b9654d4d9fc9a4ca52f1630e9f44223515b
Jose Cerqueira <jose@origensmedia.com>
2019-06-12 20:40:53 +0100

    Fix some bugs with cors and nav

--- components/Nav.js
+++ components/Nav.js
@@ -22,7 +22,7 @@ const Nav = () => (
 						<Link href="/orders">
 							<a>Orders</a>
 						</Link>
-						<Link href="/me">
+						<Link href="/">
 							<a>Account</a>
 						</Link>
 						<Signout />
